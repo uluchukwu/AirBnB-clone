cb2763b0c8e4d996dfedb12c91425565c4898119
Samson Wayne <smuhadia4@gmail.com>
2024-04-18 15:41:47 -0700

    corrected models

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """__init__magic method for models
-    creats a unique Filestorage instance for the application"""
-    from models.engine.file_storage import Filestorage
-    
+    creates a unique FileStorage instance for the application"""
+from models.engine.file_storage import FileStorage
 
-    storage = Filestorage()
-    storage.relload()
+storage = FileStorage()
+storage.reload()
+
